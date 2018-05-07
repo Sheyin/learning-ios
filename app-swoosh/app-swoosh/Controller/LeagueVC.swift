c3ef8d6d847988f3da58f9f882c2c8654b6db29e
Sheyin <Sheyin@users.noreply.github.com>
2018-05-07 00:45:59 -0500

    Lesson 47 - Passing data between view controllers / finished app

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -37,6 +37,12 @@ class LeagueVC: UIViewController {
         nextBtn.isEnabled = true
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
