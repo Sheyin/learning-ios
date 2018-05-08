2a49c8be93392b051201e575c1f9b9ab202a7bfd
Sheyin <Sheyin@users.noreply.github.com>
2018-05-07 20:42:21 -0500

    Lesson 52: Input accessory views & IBDesignable

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // programmatically creating a UIButton and attaching it to the keyboard
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5452184447, blue: 0.2364891187, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        // setting this function as the accessory view
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate() {
+        print("calculate ran!")
     }
 
 
--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    // This function + @IBDesignable let you preview changes in interface builder instead of having to build and see at runtime
+    // Everything you want to see happen in interface builder has to be in prepareForInterfaceBuilder (or function it calls)
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     // doing this in awakeFromNib() so that it is called immediately
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+        }
+        
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2465753425)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -21,8 +32,7 @@ class CurrencyTxtField: UITextField {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        }
-        
+    }
         
         
     }
